36515c55bf42f3e721e1db9c14a91ce1ee7b9b14
Yu Guo <elason4ever@gmail.com>
2025-08-21 01:55:06 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+OPENAI_API_KEY = "your-open-api-key"
+PINECONE_API_KEY = "your-pinecone-api-key"
+PINECONE_ENVIRONMENT = "your-pinecone-environment-name"
+PINECONE_INDEX_NAME = "your-pinecone-index-name"
+
+EMBEDDING_MODEL = "text-embedding-ada-002"
+SPLITTER_CHUNK_SIZE = 60
+SPLITTER_CHUNK_OVERLAP = 15
+UPLOAD_BATCH_SIZE = 32
